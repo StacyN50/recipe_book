6685146ad28e851264e2136f9923576130115a4b
StacyN50 <s.stacyotwoma50@gmail.com>
2026-05-27 16:36:58 +0300

    Refactor database connection in db.php
    Updated database connection details and error handling.

--- config/db.php
+++ config/db.php
@@ -1,16 +1,30 @@
 <?php
 
-$config = require __DIR__ . "/env.php";
+// Database configuration
+$dbhost = "dpg-d8bc526gvqtc73afk7fg-a.oregon-postgres.render.com";
+$dbname = "recipe_book_91m6";
+$dbuser = "recipe_book_91m6_user";
+$dbpass = "iE06NhBmqIcVIbAZBgrJvLWmT34UhCb3";
+$dbport = "5432";
 
 try {
+
+    // PostgreSQL PDO connection
     $conn = new PDO(
-        "pgsql:host={$config['host']};port={$config['port']};dbname={$config['name']}",
-        $config['user'],
-        $config['pass']
+        "pgsql:host=$dbhost;port=$dbport;dbname=$dbname",
+        $dbuser,
+        $dbpass
     );
 
+    // Enable error handling
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    echo "Database connected successfully";
+
 } catch (PDOException $e) {
-    die("DB Connection failed: " . $e->getMessage());
+
+    die("Connection failed: " . $e->getMessage());
+
 }
+
+?>
