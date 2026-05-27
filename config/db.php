8a112414eaaae147f5844f9885861e6c45ec22fc
StacyN50 <s.stacyotwoma50@gmail.com>
2026-05-27 14:08:15 +0300

    Refactor database configuration to use environment variables

--- config/db.php
+++ config/db.php
@@ -6,18 +6,18 @@ DATABASE CONFIGURATION
 ========================================
 */
 
-$host = get_env("localhost");
-$username = get_env("root");
-$password = get_env("");
-$database = get_env("recipe_book");
-
+$host = get_env("DB_HOST") ?: "localhost";
+$username = get_env("DB_USER") ?: "root";
+$password = get_env("DB_PASS") ?: "";
+$database = get_env("DB_NAME") ?: "recipe_db";
+$port = getenv("DB_PORT") ?: 3306;
 /*
 ========================================
 CREATE CONNECTION
 ========================================
 */
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("DB Connection failed");
