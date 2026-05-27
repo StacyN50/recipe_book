0096f1a322fc40273b31db0023cb7e47ad98e3c6
StacyN50 <s.stacyotwoma50@gmail.com>
2026-05-27 14:42:49 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,24 +1,9 @@
-<?php
+$config = require __DIR__ . "/env.php";
 
-/*
-========================================
-DATABASE CONFIGURATION
-========================================
-*/
-
-$host = get_env("DB_HOST") ?: "localhost";
-$username = get_env("DB_USER") ?: "root";
-$password = get_env("DB_PASS") ?: "";
-$database = get_env("DB_NAME") ?: "recipe_db";
-$port = getenv("DB_PORT") ?: 3306;
-/*
-========================================
-CREATE CONNECTION
-========================================
-*/
-
-$conn = new mysqli($host, $username, $password, $database, $port);
-
-if ($conn->connect_error) {
-    die("DB Connection failed");
-}
+$conn = new mysqli(
+    $config["host"],
+    $config["user"],
+    $config["pass"],
+    $config["name"],
+    $config["port"]
+);
